f43d78380b5edd94aebc874f78557ef458619d73
sumit <sumitmessi018@gmail.com>
2023-10-11 15:23:16 +0530

    learnings

---
+++ templates.js
@@ -0,0 +1,17 @@
+let name = "messi"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+let friend = 'abhishek'
+console.log(friend)
+
+//template literals
+let boy1 = "sumit"
+let boy2 = "nikhil"
+//nikhil is a friend of sumit
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+//escape sequence character
+let fruit = "bana\"na"
+console.log(fruit)
